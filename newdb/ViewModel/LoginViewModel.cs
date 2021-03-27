cffb91b9b97d13a75654f5db18e2af275cee133b
Макс Паранич <mask.parani4@yandex.ru>
2021-03-27 18:30:47 +0300

    Commands

---
+++ newdb/ViewModel/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using newdb.Infrastructure.Commands;
+
+namespace newdb.ViewModel
+{
+    class LoginViewModel:ViewModel
+    {
+        #region Commands
+        public ICommand CloseAppCommand { get; }
+
+        private bool CanCloseAppCommandExecute(object p) => true;
+        private void OnCloseAppCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        public LoginViewModel()
+        {
+            CloseAppCommand = new RelayCommand(OnCloseAppCommandExecuted, CanCloseAppCommandExecute);
+        }
+
+    }
+}
