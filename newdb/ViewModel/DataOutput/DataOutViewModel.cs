2253572eba40d8178636d0de1c72655f188a81d0
Макс Паранич <mask.parani4@yandex.ru>
2021-04-03 13:50:03 +0300

    ViewModel

--- newdb/ViewModel/DataOutput/DataOutViewModel.cs
+++ newdb/ViewModel/DataOutput/DataOutViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using newdb.Models.Base;
@@ -10,14 +12,28 @@ namespace newdb.ViewModel.DataOutput
 {
     internal class DataOutViewModel : ViewModel
     {
+        private Int64 _Array;
+        public Int64 Array
+        {
+            get => _Array;
+            set => Set(ref _Array,value);
+        }
+        private List<Data> _Date;
         public List<Data> Date
         {
             get
             {
                 EventData EV = new EventData();
                 List<Data> datas = EV.GetEntitties();
-                List<Data> dt = datas.Where(i => i.Id > 100).ToList<Data>();
-                return dt;
+                _Date = datas.Where(i => i.Id < 1000).ToList<Data>();
+                return _Date;
+            }
+            set
+            {
+                EventData EV = new EventData();
+                List<Data> datas = EV.GetEntitties();
+                _Date = datas.Where(i => i.Id < Array).ToList<Data>();
+                return;
             }
         }
     }
